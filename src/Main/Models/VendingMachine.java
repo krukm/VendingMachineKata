e0d5470aa2d37441737748911aac209096cdbd83
Matthew Kruk <matthew.kruk@gmail.com>
2017-09-25 16:09:04 -0400

    update coinAccepted test to include coinReturn

--- src/Main/Models/VendingMachine.java
+++ src/Main/Models/VendingMachine.java
@@ -1,16 +1,30 @@
 package Main.Models;
 
+import java.util.ArrayList;
+
 public class VendingMachine {
+
+    ArrayList<Coin> coins = new ArrayList<>();
+    ArrayList<Coin> coinReturn = new ArrayList<>();
+
+    Coin penny = Coin.PENNY;
+    Coin nickel = Coin.NICKEL;
+    Coin dime = Coin.DIME;
+    Coin quarter = Coin.QUARTER;
 
     public boolean coinAccepted(Coin coin) {
         switch(coin) {
             case PENNY:
+                coinReturn.add(penny);
                 return false;
             case NICKEL:
+                coins.add(nickel);
                 return true;
             case DIME:
+                coins.add(dime);
                 return true;
             case QUARTER:
+                coins.add(quarter);
                 return true;
         }
         if (coinAccepted(coin)) return true;
--- src/Main/Test/VendingMachineTest.java
+++ src/Main/Test/VendingMachineTest.java
@@ -4,18 +4,24 @@ import Main.Models.Coin;
 import Main.Models.VendingMachine;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VendingMachineTest {
     @Test
     public void checkForValidCoin() {
         VendingMachine vm = new VendingMachine();
+        ArrayList<Coin> coins = new ArrayList<>();
+        ArrayList<Coin> coinReturn = new ArrayList<>();
+
         Coin penny = Coin.PENNY;
         Coin nickel = Coin.NICKEL;
         Coin dime = Coin.DIME;
         Coin quarter = Coin.QUARTER;
 
         assertEquals(false, vm.coinAccepted(penny));
+        assertEquals(coinReturn.contains(penny), vm.coinAccepted(penny));
         assertEquals(true, vm.coinAccepted(nickel));
         assertEquals(true, vm.coinAccepted(dime));
         assertEquals(true, vm.coinAccepted(quarter));
