0fffa847d0531b5bd11a82e875003cdbeb719bbe
Matthew Kruk <matthew.kruk@gmail.com>
2017-09-25 15:38:09 -0400

    Added test for all coins, refactored

--- src/Main/CoinTest.java
+++ src/Main/CoinTest.java
@@ -5,14 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CoinTest {
+    Coin penny = Coin.PENNY;
+    Coin nickel = Coin.NICKEL;
+    Coin dime = Coin.DIME;
+    Coin quarter = Coin.QUARTER;
     @Test
-    public void whenPennyIsSelectedGetPennyWeight(){
-        Coin penny  = Coin.PENNY;
+    public void whenACoinIsSelectedGetCoinWeight(){
         assertEquals(2.500, penny.weight);
+        assertEquals(5.000, nickel.weight);
+        assertEquals(2.268, dime.weight);
+        assertEquals(5.670, quarter.weight);
     }
     @Test
-    public void whenPennyIsSelectedGetPennyDiameter() {
-        Coin penny = Coin.PENNY;
+    public void whenACoinIsSelectedGetCoinDiameter() {
         assertEquals(0.750, penny.diameter);
+        assertEquals(0.835, nickel.diameter);
+        assertEquals(0.705, dime.diameter);
+        assertEquals(0.955, quarter.diameter);
     }
 }
